96c186631771ca25fc0d471eded0cd429f45a96d
mayorr-star <immaboat11@gmail.com>
2024-02-20 21:52:06 +0000

    imported renderMenuPage function form menuPage.js

--- src/index.js
+++ src/index.js
@@ -3,5 +3,5 @@ import "./styles/menupage.css";
 import "./styles/nav.css";
 import renderHomePage from  "./pages/homepage";
 import renderMenuPage from "./pages/menuPage";
-renderHomePage();
-// renderMenuPage();
+// renderHomePage();
+renderMenuPage();
