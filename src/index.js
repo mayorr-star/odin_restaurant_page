84b25fb4c58e9ed49986041d184c2c417fa8d378
mayorr-star <immaboat11@gmail.com>
2024-02-20 20:32:24 +0000

    imported the navbar styles

--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
-import "./style.css";
+import "./styles/homepage.css";
+import "./styles/menupage.css";
+import "./styles/nav.css";
+import renderHomePage from  "./pages/homepage";
+import renderMenuPage from "./pages/menuPage";
+renderHomePage();
+// renderMenuPage();
