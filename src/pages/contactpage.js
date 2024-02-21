f560f46ebacf805da8b5cce2a67be73eebe6d25d
mayorr-star <immaboat11@gmail.com>
2024-02-21 21:59:58 +0000

    Completed the contact page

---
+++ src/pages/contactpage.js
@@ -0,0 +1,21 @@
+import { createMenuElement } from "./menuPage";
+const contentContainer = document.getElementById("content"); 
+const header = createMenuElement("h1", "header", "Contact Us");
+const contacts = createMenuElement("article", "contacts", null);
+const email = createMenuElement("span", "email", "Email: nanasCocina@gmail.com");
+const number = createMenuElement("span", "number", "Phone Number: +233 509502507");
+const instagram = createMenuElement("span", "ig", "Instagram: Nana's_Cocina");
+const wrapper = createMenuElement("div", "wrapper", null);
+const contactsContent = createMenuElement("div", "contact_content", null);
+
+contacts.appendChild(email);
+contacts.appendChild(number);
+contactsContent.appendChild(header);
+contactsContent.appendChild(contacts);
+contacts.appendChild(instagram);
+wrapper.appendChild(contactsContent);
+const renderContactPage = () => {
+    contentContainer.appendChild(wrapper);
+}
+
+export default renderContactPage;
