54c8c93daa3ef1a0d0cd3f27608c4ab1a40044aa
mayorr-star <immaboat11@gmail.com>
2024-02-23 22:01:56 +0000

    Modified

--- src/index.js
+++ src/index.js
@@ -2,9 +2,28 @@ import "./styles/homepage.css";
 import "./styles/menupage.css";
 import "./styles/contactpage.css";
 import "./styles/nav.css";
-import renderHomePage from  "./pages/homepage";
-import renderMenuPage from "./pages/menuPage";
+import renderHomePage, { removeContent } from  "./pages/homepage";
+import  renderMenuPage  from "./pages/menupage";
 import renderContactPage from "./pages/contactpage";
-// renderHomePage();
-// renderMenuPage();
-renderContactPage();
+
+window.addEventListener("DOMContentLoaded", renderHomePage);
+
+const navButtons = Array.from(document.querySelectorAll(".button"));
+navButtons.forEach(button => {
+    button.addEventListener("click", e => {
+        switch (e.target.dataset.id) {
+            case "home_btn":
+                removeContent();
+                renderHomePage();
+                break;
+            case "menu_btn":
+                removeContent();
+                renderMenuPage();
+                console.log(e.target.dataset.id)
+                break;
+            default:
+                removeContent();
+                renderContactPage()
+        }
+    })
+})
