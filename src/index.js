b8e756ef719f01afc02a3bf166f04584bb8cf232
mayorr-star <immaboat11@gmail.com>
2024-02-25 16:24:11 +0000

    Added a function to show the current active page

--- src/index.js
+++ src/index.js
@@ -8,21 +8,35 @@ import renderContactPage from "./pages/contactpage";
 
 window.addEventListener("DOMContentLoaded", renderHomePage);
 
+
 const navButtons = Array.from(document.querySelectorAll(".button"));
+const showActivePage = (e) => {
+    for (const button of navButtons) {
+        if (!e.target.classList.contains("active_btn")) {
+            e.target.classList.add("active_btn");
+        };
+        if ((button.dataset.id !== e.target.dataset.id) && button.classList.contains("active_btn")) {
+            button.classList.remove("active_btn");
+        }
+    }
+}
 navButtons.forEach(button => {
     button.addEventListener("click", e => {
         switch (e.target.dataset.id) {
             case "home_btn":
                 removeContent();
                 renderHomePage();
+                showActivePage(e);
                 break;
             case "menu_btn":
                 removeContent();
                 renderMenuPage();
+                showActivePage(e);
                 break;
             default:
                 removeContent();
                 renderContactPage()
+                showActivePage(e);
         }
     })
 })
