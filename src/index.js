63d4c49a1a37968968831a16351802d24ec94b24
mayorr-star <immaboat11@gmail.com>
2024-02-25 16:33:59 +0000

    Show active page once the page loads

--- src/index.js
+++ src/index.js
@@ -2,41 +2,51 @@ import "./styles/homepage.css";
 import "./styles/menupage.css";
 import "./styles/contactpage.css";
 import "./styles/nav.css";
-import renderHomePage, { removeContent } from  "./pages/homepage";
-import  renderMenuPage  from "./pages/menuPage";
+import renderHomePage, { removeContent } from "./pages/homepage";
+import renderMenuPage from "./pages/menuPage";
 import renderContactPage from "./pages/contactpage";
-
-window.addEventListener("DOMContentLoaded", renderHomePage);
-
 
 const navButtons = Array.from(document.querySelectorAll(".button"));
 const showActivePage = (e) => {
-    for (const button of navButtons) {
-        if (!e.target.classList.contains("active_btn")) {
-            e.target.classList.add("active_btn");
-        };
-        if ((button.dataset.id !== e.target.dataset.id) && button.classList.contains("active_btn")) {
-            button.classList.remove("active_btn");
-        }
+  for (const button of navButtons) {
+    if (!e.target.classList.contains("active_btn")) {
+      e.target.classList.add("active_btn");
     }
-}
-navButtons.forEach(button => {
-    button.addEventListener("click", e => {
-        switch (e.target.dataset.id) {
-            case "home_btn":
-                removeContent();
-                renderHomePage();
-                showActivePage(e);
-                break;
-            case "menu_btn":
-                removeContent();
-                renderMenuPage();
-                showActivePage(e);
-                break;
-            default:
-                removeContent();
-                renderContactPage()
-                showActivePage(e);
-        }
-    })
-})
+    if (
+      button.dataset.id !== e.target.dataset.id &&
+      button.classList.contains("active_btn")
+    ) {
+      button.classList.remove("active_btn");
+    }
+  }
+};
+
+navButtons.forEach((button) => {
+  button.addEventListener("click", (e) => {
+    switch (e.target.dataset.id) {
+      case "home_btn":
+        removeContent();
+        renderHomePage();
+        showActivePage(e);
+        break;
+      case "menu_btn":
+        removeContent();
+        renderMenuPage();
+        showActivePage(e);
+        break;
+      default:
+        removeContent();
+        renderContactPage();
+        showActivePage(e);
+    }
+  });
+});
+
+window.addEventListener("DOMContentLoaded", () => {
+    for (const button of navButtons){
+        if (button.dataset.id === "home_btn"){
+            button.classList.add("active_btn");
+        };
+    };
+  return renderHomePage();
+});
