53ced93b9eb55dbfcc23a47e24c962bf090ba2ee
mayorr-star <immaboat11@gmail.com>
2024-02-25 17:45:50 +0000

    imported the images

--- src/data.js
+++ src/data.js
@@ -1,75 +1,88 @@
+import brownies from "./styles/images/brownies.jpeg";
+import banku from "./styles/images/banku and tilapia.jpeg";
+import beans from "./styles/images/beans.jpeg";
+import redVelvetCake from "./styles/images/Red Velvet Cake.jpeg";
+import pumpkinPie from "./styles/images/pumpkin pie.jpeg";
+import bananaPudding from "./styles/images/banana.jpeg";
+import jollof from "./styles/images/jollof.jpg";
+import frenchFries from "./styles/images/french fries.jpeg";
+import smashedPotatoes from "./styles/images/Smashed Potatoes.jpeg";
+import creamedSpinach from "./styles/images/creamed spinach.jpeg";
+import friedRice from "./styles/images/fried rice.jpeg";
+import waakye from "./styles/images/waakye.jpeg";
+
 const  menuData = [
     {
        name: "Brownies",
        price: "$12.00",
        type: "dessert",
-       imageSrc: "./styles\images\brownies.jpeg"
+       imageSrc: brownies
     },
     {
        name: "Red Velvet Cake",
        price: "$12.00",
        type: "dessert",
-       imageSrc: "./styles\images\Red Velvet Cake.jpeg"
+       imageSrc: redVelvetCake
     },
     {
        name: "Pumpkin Pie",
        price: "$12.00",
        type: "dessert",
-       imageSrc: "./styles\images\pumpkin pie.jpeg"
+       imageSrc: pumpkinPie
     },
     {
        name: "Banana Pudding",
        price: "$12.00",
        type: "dessert",
-       imageSrc: "./styles\images\banana.jpeg"
+       imageSrc: bananaPudding
     },
     {
        name: "Jollof",
        price: "$12.00",
        type: "main_dish",
-       imageSrc: "./styles\images\jollof.jpeg"
+       imageSrc: jollof
     },
     {
        name: "Banku and Tilapia",
        price: "$12.00",
        type: "main_dish",
-       imageSrc: "./styles\images\banku and tilapia.jpeg"
+       imageSrc: banku
     },
     {
        name: "Beans and Plantain",
        price: "$12.00",
        type: "main_dish",
-       imageSrc: "./styles\images\beans.jpeg"
+       imageSrc: beans
     },
     {
        name: "French Fries",
        price: "$12.00",
        type: "side_dish",
-       imageSrc: "./styles\images\french fries.jpeg"
+       imageSrc: frenchFries
     },
     {
        name: "Smashed Potatoes",
        price: "$12.00",
        type: "side_dish",
-       imageSrc: "./styles\images\Smashed Potatoes.jpeg"
+       imageSrc: smashedPotatoes
     },
     {
        name: "Creamed Spinach",
        price: "$12.00",
        type: "side_dish",
-       imageSrc: "./styles\images\creamed spinach.jpeg"
+       imageSrc: creamedSpinach
     },
     {
        name: "Fried Rice",
        price: "$12.00",
        type: "side_dish",
-       imageSrc: "./styles\images\french fries.jpeg"
+       imageSrc: friedRice
     },
     {
        name: "Waakye",
        price: "$12.00",
        type: "main_dish",
-       imageSrc: "./styles\images\waakye.jpeg"
+       imageSrc: waakye
     },
 ]
 export default menuData;
